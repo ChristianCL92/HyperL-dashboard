fc673ec47c998d90160eb6513df5ff6bced245ec
ChristianCL92 <chrisdev92@outlook.com>
2026-06-02 08:41:10 +0200

    configure hyperliquid sdk client and subscription client

---
+++ lib/hyperliquid/client.ts
@@ -0,0 +1,16 @@
+import {
+  InfoClient,
+  HttpTransport,
+  WebSocketTransport,
+  SubscriptionClient,
+} from "@nktkas/hyperliquid";
+
+const isTestnet = process.env.NEXT_PUBLIC_HL_NETWORK !== "mainnet";
+
+export const infoClient = new InfoClient({
+  transport: new HttpTransport({ isTestnet }),
+});
+
+const subscribe = new SubscriptionClient({
+  transport: new WebSocketTransport({ isTestnet }),
+});
