58f026bc96a035ea72bea3e9721083e80024c4ea
ChristianCL92 <chrisdev92@outlook.com>
2026-06-04 09:39:59 +0200

    renaming to Provider component and include ReactQueryDevTools

--- provider/QueryProvider.tsx
+++ provider/QueryProvider.tsx
@@ -1,8 +1,9 @@
 "use client";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { ReactQueryDevtools } from "@tanstack/react-query-devtools";
 import { useState, type ReactNode } from "react";
 
-const QueryProvider = ({ children }: { children: ReactNode }) => {
+const Provider = ({ children }: { children: ReactNode }) => {
   const [queryClient] = useState(
     () =>
       new QueryClient({
@@ -15,8 +16,11 @@ const QueryProvider = ({ children }: { children: ReactNode }) => {
       }),
   );
   return (
-    <QueryClientProvider client={queryClient}>{children}</QueryClientProvider>
+    <QueryClientProvider client={queryClient}>
+      {children}
+      <ReactQueryDevtools initialIsOpen={false} />
+    </QueryClientProvider>
   );
 };
 
-export default QueryProvider;
+export default Provider;
